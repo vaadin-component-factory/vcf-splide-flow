e1336c845b02b68a9708688a7e5038e3684ff4f1
Paola De Bartolo <paola.debartolo@gmail.com>
2022-03-30 15:32:10 -0300

    refactor(demo): add more demo options

--- vcf-splide-demo/src/main/java/com/vaadin/componentfactory/demo/SplideDemoView.java
+++ vcf-splide-demo/src/main/java/com/vaadin/componentfactory/demo/SplideDemoView.java
@@ -1,57 +1,76 @@
-/*
- * Copyright 2000-2017 Vaadin Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- */
 package com.vaadin.componentfactory.demo;
 
 import com.vaadin.componentfactory.addons.splide.ImageSlide;
 import com.vaadin.componentfactory.addons.splide.Splide;
 import com.vaadin.componentfactory.addons.splide.VideoSlide;
 import com.vaadin.componentfactory.addons.splide.VideoType;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.demo.DemoView;
 import com.vaadin.flow.router.Route;
 import java.util.Arrays;
 
 /**
  * View for {@link Splide} demo.
- *
- * @author Vaadin Ltd
  */
 @Route("")
 public class SplideDemoView extends DemoView {
 
   @Override
   public void initView() {
-    createBasicSliderDemo();    
+    createImageSliderDemo();
+    createImageAndVideoSliderDemo();
   }
 
-  private void createBasicSliderDemo() {
+  private void createImageSliderDemo() {
 
     // begin-source-example
-    // source-example-heading: Simple demo
-    
-    ImageSlide imageSlide1 = new ImageSlide("https://source.unsplash.com/random/1000x1000?sig=1");
-    ImageSlide imageSlide2 = new ImageSlide("https://source.unsplash.com/random/1000x1000?sig=2");
-    ImageSlide imageSlide3 = new ImageSlide("https://source.unsplash.com/random/1000x1000?sig=3");
-    VideoSlide videoSlide1 = new VideoSlide("https://www.youtube.com/watch?v=cdz__ojQOuU", VideoType.YOUTUBE, "https://source.unsplash.com/random/1000x500?sig=4");
-    
-    Splide slider = new Splide(Arrays.asList(imageSlide1, imageSlide2, imageSlide3, videoSlide1));
-    slider.setId("simple-slider-demo");
-    slider.setWidth("400px");
+    // source-example-heading: Images slider demo
+    ImageSlide slide1 = new ImageSlide("images/slide_1.jpg");
+    ImageSlide slide2 = new ImageSlide("images/slide_2.png");
+    ImageSlide slide3 = new ImageSlide("https://source.unsplash.com/random/1000x1000?sig=3");
+
+    Splide slider = new Splide(Arrays.asList(slide1, slide2, slide3));
+    slider.setId("images-slider-demo");
+    slider.setWidth("450px");
     slider.setHeight("300px");
-    
+    slider.getElement().getStyle().set("margin", "auto");
+
     // end-source-example
-    
-    addCard("Simple demo", slider);
+
+    addCard("Images slider demo", slider);
+  }
+
+  private void createImageAndVideoSliderDemo() {
+
+    // begin-source-example
+    // source-example-heading: Images and videos slider demo
+
+    HorizontalLayout sliderLayoutContainer = new HorizontalLayout();
+    sliderLayoutContainer.setSpacing(false);
+    sliderLayoutContainer.setWidth("600px");
+    sliderLayoutContainer.setHeight("400px");
+    sliderLayoutContainer.getElement().getStyle().set("margin", "auto");
+
+    ImageSlide slide1 = new ImageSlide("images/slide_1.png");
+    ImageSlide slide2 = new ImageSlide("images/slide_2.png");
+    ImageSlide slide3 = new ImageSlide("images/slide_3.png");
+    VideoSlide slide4 = new VideoSlide("https://www.youtube.com/watch?v=C78LjVQhejI",
+        VideoType.YOUTUBE, "images/slide_4.png");
+    ImageSlide slide5 = new ImageSlide("images/slide_5.png");
+    ImageSlide slide6 = new ImageSlide("images/slide_6.png");
+    ImageSlide slide7 = new ImageSlide("images/slide_7.png");
+
+    Splide slider =
+        new Splide(Arrays.asList(slide1, slide2, slide3, slide4, slide5, slide6, slide7));
+    slider.setId("images-videos-slider-demo");
+    slider.setWidthFull();
+    slider.setHeightFull();
+
+    sliderLayoutContainer.add(slider);
+
+    // end-source-example
+
+    addCard("Images and videos slider demo", sliderLayoutContainer);
   }
 
 }
